772cc6185502ca8316ce364fcdf1361572bd0491
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2022-04-09 18:57:16 +0700

    Add project files.

---
+++ Events.web/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+//using Events.Data;
+
+namespace Events.web.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IDbSet<Category> Categories { get; set; }
+
+        public IDbSet<Department> Departments { get; set; }
+
+        //public IDbSet<CourseCategory> CourseCategories { get; set; }
+
+        //public IDbSet<CoursesAssigned> CoursesAssigned { get; set; }
+
+        public ApplicationDbContext()
+            : base("CMSEntities", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
